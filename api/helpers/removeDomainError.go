32c5f3b12d8aa83b51a3726e748d4f977e2cc93d
lackingworth <worstface@yandex.ru>
2023-12-18 06:15:24 +0300

    v.0.0.1 fixed loading env file error in removedomainerror function

--- api/helpers/removeDomainError.go
+++ api/helpers/removeDomainError.go
@@ -1,22 +1,10 @@
 package helpers
 
-import (
-	"os"
-	"log"
-
-	"github.com/joho/godotenv"
-)
+import "os"
 
 // Prevent infinite callback abuse
 func RemoveDomainError(url string) bool {
-	err := godotenv.Load("../.env")
-
-	if err != nil {
-		log.Fatal("Error loading .env file from helper function")
-	}
-
 	genURL := GeneralizeURL(url)
-	
 	
 	if genURL == "http://" + os.Getenv("DOMAIN") {
 		return false
